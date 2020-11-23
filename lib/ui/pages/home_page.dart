c1cef9658115e2db2950925d2615ca832a0d4d2d
SuzukiKensuke <s1811349@s.tsukuba.ac.jp>
2020-11-23 10:15:32 +0900

    wip: 現在高ページ途中まで

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -7,7 +7,78 @@ class HomePage extends StatelessWidget {
     return Container(
       decoration: blueGradationBoxDecoration,
       child: Center(
-        child: Text('home page.'),
+        child: InfoCard(),
+      ),
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      ),
+      margin: const EdgeInsets.fromLTRB(40, 160, 40, 60),
+      elevation: 2,
+      shadowColor: Colors.lightBlueAccent,
+      child: Column(
+        children: [
+          FlutterLogo(
+            size: 80,
+          ),
+          CurrentBalanceText(),
+          Container(
+            child: Text(
+              '\\ 999999',
+              style: TextStyle(fontSize: 40, color: Colors.blue),
+            ),
+          ),
+          Text(
+            '※これはクローンアプリです。',
+            style: TextStyle(fontSize: 10),
+          ),
+          Divider(),
+          UserInfo(),
+        ],
+      ),
+    );
+  }
+}
+
+class CurrentBalanceText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 90),
+      decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.monetization_on_outlined,
+            color: Colors.white,
+          ),
+          Text(
+            '現在高',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class UserInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [Text('記号・番号'), Text('種別')],
       ),
     );
   }
